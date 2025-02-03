a273b4c9853e8b45a422f59e2f75faeefd3bd2e6
Vishal Bhange <vishalbhange333@gmail.com>
2025-02-03 16:53:53 +0530

    Banner changes

--- src/aboutPuja/AboutPujaBanner.jsx
+++ src/aboutPuja/AboutPujaBanner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import bannerImage from "../assets/PojaBanner1.avif";
+import bannerImage from "../assets/About-pujaBanner.png";
 
 const Banner = () => {
     return (
@@ -9,11 +9,15 @@ const Banner = () => {
                 backgroundImage: `url(${bannerImage})`,
             }}
         >
+            {/* Yellow overlay */}
+            <div className="absolute inset-0 bg-yellow-500 opacity-30"></div>
+
             {/* Content */}
             <div className="relative container mx-auto px-6 text-white flex justify-end">
-                <div className="max-w-md  ">
+                <div className="max-w-md">
                     <h1 className="font-bold text-2xl ml-44 md:text-3xl">
-                    Discover the sacred ritual that blesses every aspect of your life with the divine grace of the eight forms of Goddess Lakshmi.                    </h1>
+                        Discover the sacred ritual that blesses every aspect of your life with the divine grace of the eight forms of Goddess Lakshmi.
+                    </h1>
                 </div>
             </div>
         </div>
--- src/aboutUs/AboutUsBanner.jsx
+++ src/aboutUs/AboutUsBanner.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import bannerImage from "../assets/Puja2.jpg";
+import bannerImage from "../assets/About-usBanner.png";
 
 const Banner = () => {
     return (
         <div
-            className="relative bg-cover bg-center h-[520px] flex items-center opacity-70"
+            className="relative bg-cover bg-center h-[520px] flex items-center"
             style={{
                 backgroundImage: `url(${bannerImage})`,
             }}
         >
+            {/* Pink overlay */}
+            <div className="absolute inset-0 bg-pink-500 opacity-25"></div>
+
             {/* Content */}
             <div className="relative container mx-auto px-6 text-white flex justify-end">
-                <div className="max-w-md  ">
+                <div className="max-w-md">
                     <h1 className="font-bold text-2xl ml-44 md:text-3xl">
-                    At SSG Global, we bring you closer to the divine through authentic spiritual practices.
+                        At SSG Global, we bring you closer to the divine through authentic spiritual practices.
                     </h1>
                 </div>
             </div>
